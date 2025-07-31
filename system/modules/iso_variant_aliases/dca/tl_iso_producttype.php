040a8ba55c1b77c9399ec527032834d47a8c4ca3
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-31 09:49:52 -0400

    Update tl_iso_producttype.php

--- system/modules/iso_variant_aliases/dca/tl_iso_producttype.php
+++ system/modules/iso_variant_aliases/dca/tl_iso_producttype.php
@@ -1,6 +1,7 @@
 <?php
 
+// Update the 'default' value for 'variant_attributes' so we can set 'alias' to be a default field
 $GLOBALS['TL_DCA']['tl_iso_producttype']['fields']['variant_attributes']['default'] = array
 (
-  array('name'=>'alias', 'enabled'=>1),
+    array('name'=>'alias', 'enabled'=>1),
 );
