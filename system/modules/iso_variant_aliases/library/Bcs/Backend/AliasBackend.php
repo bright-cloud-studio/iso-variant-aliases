854454ffc7c09179d668f2b92e78ebec89682ed5
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-31 11:45:41 -0400

    Update AliasBackend.php

--- system/modules/iso_variant_aliases/library/Bcs/Backend/AliasBackend.php
+++ system/modules/iso_variant_aliases/library/Bcs/Backend/AliasBackend.php
@@ -13,52 +13,14 @@ use Contao\StringUtil;
 class AliasBackend extends Alias
 {
 
-    /**
-     * Autogenerate a product alias if it has not been set yet
-     * @param mixed
-     * @param DataContainer
-     * @return string
-     * @throws \Exception
-     */
     public function save($varValue, DataContainer $dc)
     {
-        $autoAlias = false;
-        $varValue  = (string) $varValue;
+        // Perform the default Isotope save() function
+        $varValue = parent::save($varValue, $dc);
 
-        // Generate alias if there is none
-        if ('' === $varValue) {
-            $autoAlias = true;
-            $act       = Input::get('act');
-
-            if ('edit' === $act || 'overrideAll' === $act) {
-                $varValue = (string) (Input::post('name') ?: Input::post('sku'));
-            } elseif ('editAll' === $act) {
-                $varValue = (string) (Input::post('name_'.$dc->id) ?: Input::post('sku_'.$dc->id));
-            }
-        }
-
-        if ('' === $varValue) {
-            $varValue = (string) ($dc->activeRecord->name ?: $dc->activeRecord->sku);
-        }
-
-        if ('' === $varValue) {
-            $varValue = $dc->id;
-        }
-
-        $varValue = StringUtil::standardize(strip_tags($varValue));
-
-        $objAlias = Database::getInstance()
-            ->prepare('SELECT id FROM tl_iso_product WHERE id=? OR alias=?')
-            ->execute($dc->id, $varValue)
-        ;
-
-        // Check whether the product alias exists
-        if ($objAlias->numRows > 1) {
-            if (!$autoAlias) {
-                throw new \OverflowException(sprintf($GLOBALS['TL_LANG']['ERR']['aliasExists'], $varValue));
-            }
-
-            $varValue .= '.' . $dc->id;
+        // If we have a PID, this is a variant, append the SKU to make the alias unique
+        if($dc->activeRecord->pid != 0) {
+            $varValue .= "-" . $dc->activeRecord->sku;
         }
 
         return $varValue;
