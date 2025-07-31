5564d1cfe75a31903c62967a63a89d1e12cfcc69
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-31 09:13:41 -0400

    Update and rename tl_member.php to tl_iso_product.php

--- system/modules/iso_variant_aliases/dca/tl_iso_product.php
+++ system/modules/iso_variant_aliases/dca/tl_iso_product.php
@@ -1,20 +1,19 @@
 <?php
 
-/**
-* Bright Cloud Studio's Keap Integration
-*
-* @copyright  2022 Bright Cloud Studio
-* @package    keap_integration
-* @license    GNU/LGPL
-* @filesource
-*/
 
- /* Extend the tl_user palettes */
-foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
-    $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{meetings_legend},meeting_options;', $v);
-}
+$GLOBALS['TL_DCA']['tl_iso_product']['fields']['alias']['attributes'] = array
+(
+    'legend' => 'general_legend',
+    'fixed' => true,
+    'inherit' => true,
+    'variant_fixed'=>true,
+    'systemColumn'=>true
+);
+
+
+
     
-/* Add fields to tl_user */
+/*
 $GLOBALS['TL_DCA']['tl_member']['fields']['meeting_options'] = array
 (
     'sql'                     => "varchar(255) NOT NULL default ''",
@@ -23,3 +22,4 @@ $GLOBALS['TL_DCA']['tl_member']['fields']['meeting_options'] = array
     'options'                 => array('misc_billing' => 'Misc. Billing', 'misc_travel_expenses' => 'Misc. Travel Expenses', 'editing_services' => 'Editing Services', 'manager' => 'Manager', 'parking' => 'Parking', 'test_late_cancel_first' => 'First Test Late Cancel', 'test_late_cancel_additional' => 'Additional Test Late Cancel'),								
     'eval'                    => array('multiple'=>true, 'mandatory'=>false,'tl_class'=>'clr') 
 );
+*/
