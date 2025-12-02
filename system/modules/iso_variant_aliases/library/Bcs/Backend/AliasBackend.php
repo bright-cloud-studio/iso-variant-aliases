c64b4c1b9716bea2bdc5b4e1b5c359c9e5fb1a2f
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-12-02 11:43:29 -0500

    Improve alias generation in save function
    Enhance alias uniqueness by appending SKU and lowercase SKU.

--- system/modules/iso_variant_aliases/library/Bcs/Backend/AliasBackend.php
+++ system/modules/iso_variant_aliases/library/Bcs/Backend/AliasBackend.php
@@ -17,10 +17,15 @@ class AliasBackend extends Alias
     {
         // Perform the default Isotope save() function
         $varValue = parent::save($varValue, $dc);
-
+        
         // If we have a PID, this is a variant, append the SKU to make the alias unique
         if($dc->activeRecord->pid != 0) {
-            $varValue .= "-" . $dc->activeRecord->sku;
+            
+            // If the original alias doesnt already contain a lowercase version of the SKU
+            if(!str_contains($varValue, strtolower($dc->activeRecord->sku))) {
+                // append the lowercase SKU to the alias, after an underscore
+                $varValue .= "_" . strtolower($dc->activeRecord->sku);
+            }
         }
 
         return $varValue;
